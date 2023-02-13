ccd9818b8e85ce99c6ff6cc3c8e181fec1d26759
Maycol-Mohr <maycolmohr@yahoo.com.br>
2023-02-13 20:26:14 +0100

    tentando exercicio 1 com ajuda

---
+++ src/Domains/Car.ts
@@ -0,0 +1,31 @@
+import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
+
+class Car extends Vehicle {
+  private doorsQty: number;
+  private seatsQty: number;
+  
+  constructor(car: ICar) {
+    super(car.model, car.year, car.color, false, car.buyValue, car.id);
+    this.doorsQty = car.doorsQty;
+    this.seatsQty = car.seatsQty;
+  }
+   
+  public getDoorsQty(): number {
+    return this.doorsQty;
+  }
+  
+  public setDoorsQty(value: number) {
+    this.doorsQty = value;
+  }
+  
+  public getSeatsQty(): number {
+    return this.seatsQty;
+  }
+  
+  public setSeatsQty(value: number) {
+    this.seatsQty = value;
+  }
+}
+  
+export default Car;
